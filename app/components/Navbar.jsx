45f023e3afe5d726063076e5ddca0a2d60ba8c92
akosinoan <noan.babao@gmail.com>
2023-08-25 22:04:54 -0500

    fixed navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () =>{
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li className="nav-item"><Link className={`nav-link ${activeNav=='about'?'active':''}`} onClick={handleClick} href="#about">About</Link></li>
-                    <li className="nav-item"><Link className={`nav-link ${activeNav=='about'?'active':''}`} onClick={handleClick} href="#story">Story</Link></li>
+                    <li className="nav-item"><Link className={`nav-link ${activeNav=='story'?'active':''}`} onClick={handleClick} href="#story">Story</Link></li>
                     
                     <li className="nav-item"><Link className={`nav-link ${activeNav=='products'?'active':''}`} onClick={handleClick} href="#products">Products</Link></li>
                     <li className="nav-item"><Link className={`nav-link ${activeNav=='branches'?'active':''}`} onClick={handleClick} href="#branches">Branches</Link></li>
